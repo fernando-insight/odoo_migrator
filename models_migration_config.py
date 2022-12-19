b1a425f1a5a0923083da57658d13fe5f47a4566d
fernando-insight <fernando@insightsoftworks.io>
2022-12-19 11:42:03 -0300

    Moved the import & export scripts to a jupyer notebook

--- models_migration_config.py
+++ models_migration_config.py
@@ -22,7 +22,7 @@ models_migration_config = {
         'fields': ['id', 'name', 'active', 'user_id/id', 'alias_name', 'use_quotations', 'use_opportunities', 'use_leads', 'alias_contact', 'invoiced_target'],
         'domain': ['|', ['active', '=', True], ['active', '=', False]],
     },
-     'crm.lead': {
+    'crm.lead': {
         'fields': ['id', 'active', 'name', 'description', 'type', 'date_open', 'date_closed', 'message_bounce', 'priority', 'planned_revenue', 'probability', 'date_deadline', 'partner_name', 'street', 'street2', 'city', 'state_id/id', 'zip', 'country_id/id', 'website', 'partner_id/id', 'user_id/id', 'stage_id/id', 'team_id/id', 'tag_ids/id'],
         'domain': ['|', ['active', '=', True], ['active', '=', False]],
         'group_by': 'stage_id/id',
@@ -34,15 +34,6 @@ models_migration_config = {
     },
     'mail.tracking.value': {
         'fields': ['id', 'field', 'field_desc', 'field_type', 'mail_message_id/id', 'old_value_char', 'old_value_datetime', 'old_value_integer', 'old_value_monetary', 'old_value_float', 'old_value_text', \
-            'new_value_char', 'new_value_datetime', 'new_value_integer', 'new_value_monetary', 'new_value_float', 'new_value_text'],
+                   'new_value_char', 'new_value_datetime', 'new_value_integer', 'new_value_monetary', 'new_value_float', 'new_value_text'],
     }
-
 }
-
-GENERATED_CSV_FILES_PATH = 'generated_csv_files/'
-INPUT_CSV_FIILES_PATH = 'input_csv_files/'
-
-partners_without_name_file_name = 'res.partner(no name).csv'
-res_users_groups_file_name = 'res.users(groups).csv'
-crm_team_members_file_name = 'crm.team(members).csv'
-states_remapping_file_name = 'States Remapping - Input.csv'
