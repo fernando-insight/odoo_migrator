0f5a9e8306f4955f5a83b4f4424b55c3cc1523e3
fernando-insight <fernando@insightsoftworks.io>
2022-12-22 19:39:41 -0300

    Projects module migration

--- models_migration_config.py
+++ models_migration_config.py
@@ -28,6 +28,23 @@ models_migration_config = {
         'group_by': 'stage_id/id',
         'workers': 1
     },
+    'project.tags': {
+        'fields': ['id', 'name']
+    },
+    'project.project': {
+        'fields': ['id', 'active', 'name', 'color', 'user_id/id', 'sequence', 'privacy_visibility', 'partner_id/id', 'label_tasks', 'alias_name', 'alias_contact', 'resource_calendar_id/id'],
+        'workers': 1,
+        'domain': ['|', ['active', '=', True], ['active', '=', False]],
+    },
+    'project.task.type': {
+        'fields': ['id', 'name', 'description', 'fold', 'sequence', 'legend_blocked', 'legend_done', 'legend_normal', 'project_ids/id']
+    },
+    'project.task': {
+        'fields': ['id', 'active', 'name', 'priority', 'description', 'color', 'stage_id/id', 'email_from', 'project_id/id', 'user_id/id', 'date_deadline', 'tag_ids/id', 'planned_hours', 'sequence', 'partner_id/id', 'date_assign', 'date_last_stage_update', 'parent_id/id'],
+        'ignore_fields': ['parent_id/id'],
+        'domain': ['|', ['active', '=', True], ['active', '=', False]],
+        'workers': 1
+    },
     'mail.message': {
         'fields': ['id', 'res_id', 'model', 'message_type', 'body', 'subtype_id/id', 'message_id', 'subject', 'date', 'email_from', 'author_id/id', 'record_name', 'partner_ids/id', 'parent_id/id'],
         'domain': [['model', 'in', ['res.partner', 'crm.lead', 'crm.team', 'crm.stage']]],
@@ -35,5 +52,8 @@ models_migration_config = {
     'mail.tracking.value': {
         'fields': ['id', 'field', 'field_desc', 'field_type', 'mail_message_id/id', 'old_value_char', 'old_value_datetime', 'old_value_integer', 'old_value_monetary', 'old_value_float', 'old_value_text', \
                    'new_value_char', 'new_value_datetime', 'new_value_integer', 'new_value_monetary', 'new_value_float', 'new_value_text'],
-    }
+    },
+    'ir.attachment': {
+        'fields': ['id', 'name', 'description', 'type', 'mimetype', 'create_uid/id', 'index_content', 'public', 'res_name', 'res_model', 'res_id']
+    },
 }
